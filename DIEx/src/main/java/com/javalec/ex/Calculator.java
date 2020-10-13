ee9a61b286138c9548050503200787fe0318ede2
unknown <werellel@gmail.com>
2020-10-13 21:46:00 +0900

    lec 2: DI

---
+++ DIEx/src/main/java/com/javalec/ex/Calculator.java
@@ -0,0 +1,26 @@
+package com.javalec.ex;
+
+public class Calculator {	
+	public void add(int firstNum, int secondNum) {
+		System.out.println("add");
+		System.out.println((firstNum + secondNum));
+	}
+	
+	public void sub(int firstNum, int secondNum) {
+		System.out.println("sub");
+		System.out.println((firstNum - secondNum));
+		
+	}
+	
+	public void mul(int firstNum, int secondNum) {
+		System.out.println("mul");
+		System.out.println((firstNum * secondNum));
+			
+		}
+		
+	public void div(int firstNum, int secondNum) {
+		System.out.println("mul");
+		System.out.println((firstNum / secondNum));
+		
+	}
+}
