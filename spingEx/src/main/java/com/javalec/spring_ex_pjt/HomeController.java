085dbe3905de968bb605ffb9fa0e5d51d91bd1ed
unknown <werellel@gmail.com>
2020-10-18 23:48:31 +0900

    spring-MVC : form

--- spingEx/src/main/java/com/javalec/spring_ex_pjt/HomeController.java
+++ spingEx/src/main/java/com/javalec/spring_ex_pjt/HomeController.java
@@ -63,4 +63,18 @@ public class HomeController {
 		return "member/join";
 	}
 	
+	@RequestMapping("index")
+	public String goIndex() {
+		return "index";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value="/student")
+	public String goStudent(HttpServletRequest httpServletRequest, Model model) {
+		System.out.println("RequestMethod.GET");
+		
+		String id = httpServletRequest.getParameter("id");
+		System.out.println("id :" + id);
+		model.addAttribute("studentId", id);
+		return "student/studentId";
+	}
 }
