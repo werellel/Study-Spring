3a9e56df61dd0421af16688797d2ea2983cfdccf
unknown <werellel@gmail.com>
2020-10-25 19:16:50 +0900

    Add 'reply' sql and Spring Code

--- sping-board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
+++ sping-board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
@@ -16,7 +16,40 @@ import com.javalec.spring_pjt_board.dto.BDto;
 public class BDao {
 	
 	DataSource dataSource;
-	
+	public void reply(String bId, String  bName, String bTitle, String bContent, String bGroup, String bStep, String bIndent) {
+		// TODO Auto-generated method stub
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			connection = dataSource.getConnection();
+			String query = "insert into mvc_board (bId, bName, bTitle, bContent, bGroup, bStep, bIndent) values (mvc_board_seq.nextval, ? ,? ,? ,? ,? ,?)";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, bName);
+			preparedStatement.setString(2, bTitle);
+			preparedStatement.setString(3, bContent);
+			preparedStatement.setInt(4, Integer.parseInt(bGroup));
+			preparedStatement.setInt(5, Integer.parseInt(bStep));
+			preparedStatement.setInt(6, Integer.parseInt(bIndent));
+			
+			int rn = preparedStatement.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				if(preparedStatement != null) preparedStatement.close();
+				if(connection != null) connection.close();
+				
+			} catch (Exception e2) {
+				e2.printStackTrace();
+
+			}
+		}
+
+	}
 	public void delete(String strID) {
 		// TODO Auto-generated method stub
 		Connection connection = null;
@@ -41,6 +74,49 @@ public class BDao {
 			}
 		}
 		
+	}
+	
+	public BDto reply_view(String strID) {
+		BDto dto = null;
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = dataSource.getConnection();
+			String query = "select * from mvc_board where bid = ?";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, Integer.parseInt(strID));
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()) {
+				int bId = resultSet.getInt("bId");
+				String bName = resultSet.getString("bName");
+				String bTitle = resultSet.getString("bTitle");
+				String bContent = resultSet.getString("bContent");
+				Timestamp bDate = resultSet.getTimestamp("bDate");
+				int bHit = resultSet.getInt("bInt");
+				int bGroup = resultSet.getInt("bGroup");
+				int bStep = resultSet.getInt("bStep");
+				int bIndent = resultSet.getInt("bIndent");
+				
+				dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			
+		} finally {
+			try {
+				if(resultSet != null) resultSet.close();
+				if(preparedStatement != null) preparedStatement.close();
+				if(connection != null) connection.close();
+				
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return null;
 	}
 	
 	public void modify(String bId, String bName, String bTitle, String bContent) {
